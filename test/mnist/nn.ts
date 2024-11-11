3344096489e6324c67a6436a6a263dd755f4ff06
Shelwin <shelwinsunga@gmail.com>
2024-11-10 20:10:54 -0800

    save

--- test/mnist/nn.ts
+++ test/mnist/nn.ts
@@ -20,6 +20,23 @@ type Sigmoid<A extends number> = Div<
     Add<1, Exp<E, Mul<A, -1>>>
 >;
 
+type FillArray<
+  Count extends number,
+  Value,
+  Array extends any[]
+> = FillArrayImpl<Count, 0, Value, Array>;
+
+type FillArrayImpl<
+  Count extends number,
+  I extends number,
+  Value,
+  Array extends any[]
+> = I extends Count
+  ? Array
+  : FillArrayImpl<Count, Add<I, 1>, Value, [Value, ...Array]>;
+
+
+
 // Original array builder for smaller chunks
 type MakeArray<N extends number> = N extends 1 ? [Rand<0, 1>] : MakeArrayImpl<N, 0, []>;
 
@@ -30,4 +47,27 @@ type MakeArrayImpl<
   Acc extends number[]
 > = N extends I ? [...Acc, Rand<0, 1>] : MakeArrayImpl<N, Add<I, 1>, [...Acc, Rand<0, 1>]>;
 
-type Main<Args extends string[]> = Print<MakeArray<768>>;
+// Compute dot product recursively
+// dot product is the sum of the products of the corresponding elements of the two sequences of numbers.
+type DotProduct<
+    A extends number[],
+    B extends number[],
+    Sum extends number = 0
+> = A extends [infer X extends number, ...infer RestA extends number[]]
+    ? B extends [infer Y extends number, ...infer RestB extends number[]]
+        ? DotProduct<RestA, RestB, Add<Sum, Mul<X, Y>>>
+        : Sum
+    : Sum;
+
+// Test neuron activation with small arrays
+type TestInput = [0.5, 0.3, 0.2];
+type TestWeights = [0.4, 0.6, 0.2];
+
+// type NeuronActivation<
+//     Input extends number[],
+//     Weights extends number[],
+//     Bias extends number
+// > = Sigmoid<Add<DotProduct<Input, Weights>, Bias>>;
+
+
+type Main<Args extends string[]> = Print<5>;
